db183ddbc466c4eccb6fc20f846751695b6ec58f
Muhammad Eryawan <eryawanir@gmail.com>
2023-12-07 20:21:22 +0700

    Submission 4 - tambah kolom books

--- database/migrations/2023_12_07_120715_create_books_table.php
+++ database/migrations/2023_12_07_120715_create_books_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150);
+            $table->string('author', 100);
+            $table->string('publisher', 80);
+            $table->date('published_at');
             $table->timestamps();
         });
     }
